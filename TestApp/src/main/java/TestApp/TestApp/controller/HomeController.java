0a6afced74e7e0a51353d6efced6a4084448f2d2
Mickey Navarro <37810413+MickeyNavarro@users.noreply.github.com>
2021-01-10 23:01:33 -0700

    add paths to login/registration

--- TestApp/src/main/java/TestApp/TestApp/controller/HomeController.java
+++ TestApp/src/main/java/TestApp/TestApp/controller/HomeController.java
@@ -1,3 +1,9 @@
+//Almicke Navarro 
+//CST-323
+//January 10, 2020 
+//This is my own work.
+
+//HomeController will deal with accessing the home page, which includes login & registration
 package TestApp.TestApp.controller;
 
 import java.io.IOException;
@@ -6,13 +12,30 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import model.PlayerModel;
 
 @Controller
 public class HomeController {
 
+	//home page 
 	@RequestMapping(value="/")
 	public ModelAndView test(HttpServletResponse response) throws IOException{
 		return new ModelAndView("home");
+	}
+	
+	//login page
+	@RequestMapping(value="/login", method = RequestMethod.GET) 
+	public ModelAndView displayLoginPage(HttpServletResponse response) throws IOException{
+		//return new ModelAndView("login", "player", new PlayerModel(0,"", ""));
+		return new ModelAndView("login"); 
+	}
+	
+	//registration page
+	@RequestMapping(value="/register", method = RequestMethod.GET) 
+	public ModelAndView displayRegistrationPage(HttpServletResponse response) throws IOException{ 
+		return new ModelAndView("registration");
 	}
 }
