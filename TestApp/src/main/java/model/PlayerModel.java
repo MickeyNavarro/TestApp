926159d01f796eeded826f7c49f23713489d47d4
Mickey Navarro <37810413+MickeyNavarro@users.noreply.github.com>
2021-01-10 21:04:47 -0700

    update PlayerModel to include attributes

--- TestApp/src/main/java/model/PlayerModel.java
+++ TestApp/src/main/java/model/PlayerModel.java
@@ -1,5 +1,57 @@
+//Almicke Navarro 
+//CST-323
+//January 10, 2020 
+//This is my own work.
+
+//PlayerModel will contain the Player attributes
 package model;
 
-public class PlayerModel {
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+public class PlayerModel {
+	
+	private int ID; 
+	
+	@NotNull(message = "Name cannot be null.")
+	@Size(min=3, max=30, message="Name must be between 3 and 45 characters.")
+	private String Name;
+	
+	@NotNull(message = "Password cannot be null.")
+	@Size(min=3, max=30, message="Password must be between 3 and 30 characters.")
+	private String Password;
+
+	public PlayerModel(int iD, String name, String password) {
+		super();
+		ID = iD;
+		Name = name;
+		Password = password;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getPassword() {
+		return Password;
+	}
+
+	public void setPassword(String password) {
+		Password = password;
+	} 
+	
+	
+	
 }
